69effeb59e7c2acdded38e83a61600bca07d0bac
Yuz <wwowkdrr@gmail.com>
2025-06-09 11:27:42 +0800

    update()更新單筆資料

--- index.php
+++ index.php
@@ -16,8 +16,17 @@
 
 // $all=q("select name, price from items order by price");
 
-dd(find('items',3));
+//dd(find('items',3));
+//dd(find('items',['name'=>'蛋餅','stock'=>50]));
 
+$row=find('items',5);
+dd($row);
+
+$row['cost']=15;
+$row['prove']=45;
+dd($row);
+
+update("items",$row);
 /* stars('正三角形', 15);
 stars('菱形', 15);
 stars('矩形', 15);
